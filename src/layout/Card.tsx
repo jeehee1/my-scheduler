cd1255da4b5f6e6429c50c619d0686b7f3ec952d
jeehee1 <jeehee1w@gmail.com>
2023-10-21 12:54:41 +0900

    refactor: 카드 레이아웃 코드 리팩토링

--- src/layout/Card.tsx
+++ src/layout/Card.tsx
@@ -2,26 +2,17 @@ import { useContext, useEffect, useState } from "react";
 import classes from "./Card.module.css";
 import { ModeContext } from "../context/mode-context";
 
-const Card = ({ children, editting }: { children: any , editting: boolean}) => {
+const Card = ({ children, editting }: { children: any; editting: boolean }) => {
   const modeCtx = useContext(ModeContext);
-
-  // 마우스가 해당 컴포넌트 위에 위치한 경우 isActive가 true 값으로 설정
-  const [isActive, setIsActive] = useState(false);
 
   return (
     <>
       {modeCtx.editMode && (
         <div
-          onMouseOver={() => {
-            setIsActive(true);
-          }}
-          onMouseOut={() => {
-            setIsActive(false);
-          }}
-          // isActive 마우스 위치 && !editting 수정중이 아닌 경우 sping 효과
-          className={
-            isActive && !editting ? `${classes.card} ${classes.active}` : classes.card
-          }
+          // 에딧 모드이고 수정중이 아닌경우 - mouse hover시 spring 효과
+          className={`${classes.card} ${
+            modeCtx.editMode && !editting ? classes.active : ""
+          }`}
         >
           {children}
         </div>
